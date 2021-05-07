23984f7d497a396f85a54b1b00068789eecc4e1f
Luca Palermo <lucaapalermo@gmail.com>
2021-05-07 15:35:01 -0700

    Parse output of command into object

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ export default class IP {
   /**
    * Runs getNeighbors command.
    *
-   * Calls native module to run "ip neigh show" on device and returns output or error.
+   * Calls native module to run "ip neigh show" on device and returns parsed output or error.
    *
-   * @returns {Promise} Promise containing string output of command or thrown error.
+   * @returns {Promise} Promise containing parsed output of command or error.
    */
   static async getNeighbors() {
-    const result = await RNIpAndroid.getNeighbors();
+    const output = await RNIpAndroid.getNeighbors();
+
+    const result = {};
+
+    output.split('\n').forEach((line) => {
+      const parts = line.split(' ');
+
+      // ip address
+      result[parts[0]] = {
+        mac: parts[4],
+        status: parts[5] // stale/reachable/failed
+      };
+    });
 
     return result;
   }
